cc7b15ef19391c94fc455426a727557581557669
Stanley Lara <stanleylaraiii@icloud.com>
2014-09-15 00:50:08 -0700

    Changed tiles from List<string> to Array of strings. Updated output method.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,39 @@ namespace TileGamePuzzle
     {
         static void Main(string[] args)
         {
+            var tiles = new string[16];
+            for (var i = 0; i < 16; i++)
+            {
+                if (i < 15)
+                {
+                    tiles[i] = (i + 1).ToString();
+                }
+                else
+                {
+                    tiles[i] = "";
+                }
+            }
+
+            PrintGameGrid(tiles);
+            Console.ReadLine();
+        }
+
+        public static void PrintGameGrid(string[] list )
+        {
+            Console.WriteLine("╔═════════════════════════════════╗");
+            Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[0], list[1], list[2], list[3]);
+            Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
+            Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[4], list[5], list[6], list[7]);
+            Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
+            Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[8], list[9], list[10], list[11]);
+            Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
+            Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[12], list[13], list[14], list[15]);
+            Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
+            Console.WriteLine("╚═════════════════════════════════╝");
         }
     }
 }
