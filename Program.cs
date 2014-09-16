663a764c8a6eb19295a37d7c08dfdf57d7db42fb
Stanley Lara <stanleylaraiii@icloud.com>
2014-09-16 01:03:20 -0700

    Added methods to check for winner and to randomize tiles.

--- Program.cs
+++ Program.cs
@@ -1,47 +1,155 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TileGamePuzzle
 {
+    public static class BlankTile
+    {
+        public static int X { get; set; }
+        public static int Y { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            var tiles = new string[16];
-            for (var i = 0; i < 16; i++)
+            var tiles = new string[4, 4];
+
+            InitializeTileValues(tiles);
+            RandomizeTiles(tiles);
+
+            while (true)
             {
-                if (i < 15)
+                Console.Clear();
+                PrintGameGrid(tiles);
+                var cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.Q)
                 {
-                    tiles[i] = (i + 1).ToString();
+                    return;
+                }
+
+                MoveTile(cki.Key, tiles);
+                if (!IsAWinner(tiles))
+                {
+                    continue;
                 }
                 else
                 {
-                    tiles[i] = "";
+                    Console.Clear();
+                    PrintGameGrid(tiles);
+                    Console.WriteLine("You are a winner!");
+                    Console.ReadLine();
+                    return;
                 }
             }
-
-            PrintGameGrid(tiles);
-            Console.ReadLine();
         }
 
-        public static void PrintGameGrid(string[] list )
+        private static bool IsAWinner(string[,] tiles)
         {
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    if (i == 3 && j == 3) continue;
+                    if (tiles[i, j] != ((4*i) + (j + 1)).ToString())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static void RandomizeTiles(string[,] tiles)
+        {
+            var rand = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                switch (rand.Next()%4)
+                {
+                    case 0:
+                        MoveTile(ConsoleKey.LeftArrow, tiles);
+                        break;
+                    case 1:
+                        MoveTile(ConsoleKey.RightArrow, tiles);
+                        break;
+                    case 2:
+                        MoveTile(ConsoleKey.UpArrow, tiles);
+                        break;
+                    case 3:
+                        MoveTile(ConsoleKey.DownArrow, tiles);
+                        break;
+                }
+            }
+        }
+
+        private static void MoveTile(ConsoleKey key, string[,] tiles)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if ((BlankTile.Y + 1) <= 3)
+                    {
+                        tiles[BlankTile.X, BlankTile.Y] = tiles[BlankTile.X, BlankTile.Y + 1];
+                        tiles[BlankTile.X, BlankTile.Y + 1] = "";
+                        BlankTile.Y++;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if ((BlankTile.Y - 1) >= 0)
+                    {
+                        tiles[BlankTile.X, BlankTile.Y] = tiles[BlankTile.X, BlankTile.Y - 1];
+                        tiles[BlankTile.X, BlankTile.Y - 1] = "";
+                        BlankTile.Y--;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    if ((BlankTile.X + 1) <= 3)
+                    {
+                        tiles[BlankTile.X, BlankTile.Y] = tiles[BlankTile.X + 1, BlankTile.Y];
+                        tiles[BlankTile.X + 1, BlankTile.Y] = "";
+                        BlankTile.X++;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if ((BlankTile.X - 1) >= 0)
+                    {
+                        tiles[BlankTile.X, BlankTile.Y] = tiles[BlankTile.X - 1, BlankTile.Y];
+                        tiles[BlankTile.X - 1, BlankTile.Y] = "";
+                        BlankTile.X--;
+                    }
+                    break;
+            }
+        }
+
+        private static void InitializeTileValues(string[,] tiles)
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    tiles[i, j] = ((4*i) + (j + 1)).ToString();
+                }
+            }
+            tiles[BlankTile.X = 3, BlankTile.Y = 3] = "";
+        }
+
+        public static void PrintGameGrid(string[,] list )
+        {
+            Console.WriteLine("Use arrow keys to move tiles.");
+            Console.WriteLine("Enter 'Q' to quit.\n");
+
             Console.WriteLine("╔═════════════════════════════════╗");
             Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
-            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[0], list[1], list[2], list[3]);
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[0,0], list[0,1], list[0,2], list[0,3]);
             Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
             Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
-            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[4], list[5], list[6], list[7]);
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[1,0], list[1,1], list[1,2], list[1,3]);
             Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
             Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
-            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[8], list[9], list[10], list[11]);
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[2,0], list[2,1], list[2,2], list[2,3]);
             Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
             Console.WriteLine("║ ╔═════╗ ╔═════╗ ╔═════╗ ╔═════╗ ║");
-            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[12], list[13], list[14], list[15]);
+            Console.WriteLine("║ ║ {0,2}  ║ ║ {1,2}  ║ ║ {2,2}  ║ ║ {3,2}  ║ ║", list[3,0], list[3,1], list[3,2], list[3,3]);
             Console.WriteLine("║ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ║");
             Console.WriteLine("╚═════════════════════════════════╝");
         }
